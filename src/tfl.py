2210215e653b2c9131393b0310dc84426f9b47ce
Ari57 <njds7777@gmail.com>
2022-08-27 22:31:15 +0100

    Started working on Journey Planner, right now it works with a predefined location but will add option to choose station in the future

--- src/tfl.py
+++ src/tfl.py
@@ -2,13 +2,21 @@ import requests
 from secret.keys import pk, sk
 import sys
 
-url = "https://api.tfl.gov.uk/Line/Mode/tube/Status?app_id="+pk+"&app_key="+sk # enter your own keys in a seperate file and import them here
+# ask the user where they want to go, search for those stations, take ics code, give directions
+# url - journey/jr/ics/to/ics
+
+fromLocation = "goodmayes"
+toLocation = "canary wharf"
+
+url = "https://api.tfl.gov.uk/journey/journeyresults/"+fromLocation+"/to/"+toLocation+"?app_id="+pk+"&app_key="+sk # enter your own keys in a seperate file and import them here
 
 response = requests.get(url)
 
 responseJson = response.json()
 
-for release in responseJson:
-    if release["id"] == sys.argv[1]:
-        #print(release["lineStatuses"][0]["statusSeverityDescription"])
-        print(release["lineStatuses"][0]["reason"])
+# TODO: find the length of whatever [0] is, iterate over it, pick up all stop names, display stop names, allow user to pick one, convert that to ICS code
+
+test = responseJson["fromLocationDisambiguation"]["disambiguationOptions"][0]["place"]["icsCode"]
+print(test)
+test2 = responseJson["toLocationDisambiguation"]["disambiguationOptions"][1]["place"]["commonName"]
+print(test2)
