9e203767bd64e38624f920c61223abac9eb88d4e
Ari57 <njds7777@gmail.com>
2022-09-11 21:15:42 +0100

    Removed file

--- tests/startingLocation_test.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
-
-from JourneySteps import startingLocation
-
-if __name__ == "__main__":
-    startingLocation
